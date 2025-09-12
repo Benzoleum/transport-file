c9ad5577956d2d47415550967085b061cad0128a
bit <50826868+Benzoleum@users.noreply.github.com>
2025-09-12 19:28:20 +0100

    redundant lines

--- src/main/java/com/giftgo/transport_file/TransportFileApplication.java
+++ src/main/java/com/giftgo/transport_file/TransportFileApplication.java
@@ -1,19 +1,13 @@
 package com.giftgo.transport_file;
 
-import com.giftgo.transport_file.controller.InputFileController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TransportFileApplication {
-    private static final Logger logger = LoggerFactory.getLogger(TransportFileApplication.class);
 
-    public static boolean skipValidation = false;
-
-	public static void main(String[] args) {
-		SpringApplication.run(TransportFileApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TransportFileApplication.class, args);
+    }
 
 }
